52e85bec645b4d9ba5cb66d8e22226d23bd6f814
ilhamarfian <ilham.arfian@btpn.com>
2025-01-16 21:19:47 +0700

    fix import and returns

--- say_hello.go
+++ say_hello.go
@@ -1,8 +1,10 @@
 package go_say_hello
 
+import "fmt"
+
 func SayHello(message string) (string, error) {
 	if message == "" {
 		return nil, fmt.Errorf("message cannot be empty")
 	}
-	return "Hello, " + message
+	return "Hello, " + message, nil
 }
