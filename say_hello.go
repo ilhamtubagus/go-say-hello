44d10a8d29e36bd09326c0045c1c439d542f94b5
ilhamarfian <ilham.arfian@btpn.com>
2025-01-16 21:16:05 +0700

    Add validation

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,8 @@
 package go_say_hello
 
-func SayHello(message string) string {
+func SayHello(message string) (string, error) {
+	if message == "" {
+		return nil, fmt.Errorf("message cannot be empty")
+	}
 	return "Hello, " + message
 }
